7dbe88659bf819be2b0d0ef6430943287a8a6182
srjn45 <srajanpathak45@gmail.com>
2018-10-15 14:43:07 +0530

    used post method to create resource instead of put

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,16 +7,16 @@ var commentHandler = require('../handler/comments');
 /* GET all posts */
 router.route('/')
   .get(postHandler.getAllPosts)
-  .put(postHandler.addPost);
+  .post(postHandler.addPost);
 
 router.route('/:id')
   .get(postHandler.getPostById)
   .delete(postHandler.deletePostById);
 
-router.route('/:id/comment')
-  .put(commentHandler.addComment);
+router.route('/:id/comments')
+  .post(commentHandler.addComment);
 
-router.route('/:id/comment/:cid')
+router.route('/:id/comments/:cid')
   .delete(commentHandler.deleteCommentById);
 
 module.exports = router;
