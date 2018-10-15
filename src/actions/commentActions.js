40af387f2ec962fe5ca138eb5efc88e2157e7a13
srjn45 <srajanpathak45@gmail.com>
2018-10-15 14:43:58 +0530

    modified the api path reset form after successful api call

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
+import { resetForm } from "./formActions";
 
-
-export const addComment = (dispatch, pid, data) => {
-    axios.put('http://127.0.0.1:3000/api/posts/' + pid + '/comment', data).then(res => {
+// create
+export const addComment = (dispatch, pid, data, idx) => {
+    axios.post('http://127.0.0.1:3000/api/posts/' + pid + '/comments', data).then(res => {
         let response = res.data;
         if (response.status) {
             dispatch({ type: 'COMMENT_ADDED', payload: { pid: pid, res: response.payload } });
+            resetForm(dispatch, idx);
         }
     });
 }
 
+// delete
 export const deleteComment = (dispatch, pid, cid) => {
-    axios.delete('http://127.0.0.1:3000/api/posts/' + pid + '/comment/' + cid).then(res => {
+    axios.delete('http://127.0.0.1:3000/api/posts/' + pid + '/comments/' + cid).then(res => {
         let response = res.data;
         if (response.status) {
             dispatch({ type: 'COMMENT_DELETED', payload: { pid: pid, cid: cid } });
